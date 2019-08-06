8586ae3c8725f98e83328f7423974df48fc2c7fe
codeartisanacademy <49558035+codeartisanacademy@users.noreply.github.com>
2019-08-06 09:20:29 +0700

    added function add

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
 print('hello from Github')
+
+x = input('enter the first number: ')
+y = input('enter the second number: ')
+
+def add(first, second):
+  print(int(first) + int(second))
+  
+add(x, y)
+  
